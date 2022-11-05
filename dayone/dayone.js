ef0915b596204a5bcd93134476d7408ba313a340
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-05 10:27:42 +0100

    display json and html on the server

--- dayone/dayone.js
+++ dayone/dayone.js
@@ -1,11 +1,38 @@
 const http = require('http');
 const host = '127.0.0.1';
 const port = 3000;
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-     response.write("i serve plain text on the server successfully")
-    response.end("ghguthg")
-})
+const fs = require('fs')
+
+// //the server serves string
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'text/plain'})
+//      response.write("I serve plain text on the server successfully")
+//     response.end()
+// })
+
+// //The server serves html file
+//   const server = http.createServer((request, response) => {
+//      response.writeHead(200, {'Content-type' : 'text/html'})
+//      fs.readFile('index.html', (error, data) => {
+//          if(error) throw error
+//          response.end(data)
+//      })
+//   })
+
+// the server serves a json file
+//   const server = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'application/json'})
+//     let jayson = {
+//           status : 200,
+//           message: 'success',
+//           result: ['monday', 'tuesday', 'wednesday'],
+//           phone: 080
+//     }
+//       response.end(JSON.stringify(jayson))
+//   })
+
+  //the server serves a pdf file
+
 
 server.listen(port, host, (err) => {
     if(err) {
