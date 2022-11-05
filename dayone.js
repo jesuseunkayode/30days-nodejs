356558e8d54e6e1dc8d46806f4ee2c52019a1ccb
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-05 09:39:40 +0100

    created a simple server

--- dayone.js
+++ dayone.js
@@ -1,6 +1,12 @@
-const http = rquire('http');
-const hostname = '127.0.0.1';
-const PORT = 3000;
+const http = require('http');
+const host = '127.0.0.1';
+const port = 3000;
 const server = http.createServer((request, response) => {
-    response.write
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    console.log('server is working')
+
+})
+
+server.listen(port, host, (err) => {
+    console.log('server is success')
 })
