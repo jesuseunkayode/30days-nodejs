716fce17ff931ce443fb6221e12b096a6bca87b1
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-06 15:30:37 +0100

    final task completed

--- dayone/dayone.js
+++ dayone/dayone.js
@@ -32,7 +32,44 @@ const fs = require('fs')
 //   })
 
   //the server serves a pdf file
+//   const server = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'application/pdf'})
+//        fs.readFile('index.pdf', (error, data) => {
+//          if(error) {
+//             response.json({'status':'error', msg:err})
+//          } else {
+//             response.write(data)
+//             response.end()
+//          }
+//        })
+//   })
 
+   //the server serve audio file
+//    const server = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'audio/mp3'})
+//     fs.exists('audio.mp3', (exists)=> {
+//         if(exists) {
+//             let rStream = fs.createReadStream('audio.mp3')
+//             rStream.pipe(response)
+//         } else {
+//             response.end("It is 404 error")
+//         }
+//     })
+//    })
+
+//the server serves vido file
+ const server = http.createServer((request, response) =>{
+     response.writeHead(200, {'Content-Type' :'video/mp4'})
+     fs.exists('video.mp4', (exists) => {
+         if(exists) {
+            let rStream = fs.createReadStream('video.mp4')
+             rStream.pipe(response)
+         } else {
+            response.send('It is 404 error');
+            response.end();
+         }
+     })
+ })
 
 server.listen(port, host, (err) => {
     if(err) {
