d3f9f88e1f3dcb7d89ecf3de410969b49ed1dc1a
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-09 23:45:19 +0100

    update the error message

--- dayfive/index.js
+++ dayfive/index.js
@@ -1,9 +1,16 @@
+const fs = require('fs')
 //properties of error in nodejs
-const err = new Error('show error message');
-console.error(err.message)
+// const err = new Error('show error message');
+// console.error(err.message)
 
 // 4 types of errors
 // 1) Standard Javascript errors
 // 2) System errors
 // 3) User specified error
 // 4) Assertion errors
+
+fs.readFile('message.txt', 'utf8', (err, data) => {
+    if(err)
+    return console.error(err);
+   console.log("The data is ", data)
+})
