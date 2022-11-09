8b88414c075e0d618bbfdb4e49d7135aaeaf2152
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-09 23:36:49 +0100

    types of erros added

--- dayfive/index.js
+++ dayfive/index.js
@@ -1,4 +1,9 @@
 //properties of error in nodejs
-
 const err = new Error('show error message');
 console.error(err.message)
+
+// 4 types of errors
+// 1) Standard Javascript errors
+// 2) System errors
+// 3) User specified error
+// 4) Assertion errors
