130960d68613b05693d72eef7dbf091cb8bab40e
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-10 09:55:14 +0100

    practice with array method

---
+++ daysix/index.js
@@ -0,0 +1,39 @@
+// const arr = ['books', 'pen', 'laptop']
+// console.log(arr)
+// console.clear()
+// const ibile = new Array(1,2,4,5,6,7,8)
+// console.log(ibile)
+// console.clear()
+// console.log(ibile.length)
+
+//push method in array
+const book = ['make money fast', 'enjoy your day'];
+book.push('make your day count')          
+ console.log(book);
+
+ //pop method of array
+ const arr = [3,5,6,7,8,7,9];
+ arr.pop()
+ console.log(arr)
+
+ //unshift method of array
+ const laptop = ['hp', 'dell', 'macboo'];
+ laptop.unshift('samsung');
+ console.log(laptop)
+
+ 
+ //shift method of array
+ const top = ['facecap', 'shirt', 'glasses'];
+ top.shift('samsung');
+ console.log(top)
+
+  console.clear()
+  //reverse method of array
+  const change = ['naira', 'dollar', 'euro', 'pounds', 'cedis', 'franc'];
+  change.reverse();
+  console.log(change)
+
+//sort method of array
+const exp = [31,15,16,27,85,33,19];
+ exp.sort()
+ console.log(exp)
