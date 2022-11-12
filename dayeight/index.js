7a45eaae3ad54a7b3faa05ccec1566c621e53f45
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-11-12 22:55:53 +0100

    first commit of the day

---
+++ dayeight/index.js
@@ -0,0 +1,33 @@
+//Step 1: open an account with npm
+
+//Step 2: Enter your information
+
+//Step 3: Create your account
+
+const function1 = (params) => {
+    //function starts from here
+
+    //functionality here
+
+    //function ends here
+}
+
+
+const function2 = (params) => {
+    //function starts from here
+
+    //functionality here
+
+    //function ends here
+}
+
+module.exports = {
+    function1 :function1,
+    function2: function2
+}
+
+//You can use npm publish package
+ //npm publish
+
+// ignore file in the directory
+// .npmignore or .gitignore
